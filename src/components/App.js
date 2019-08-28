f6787b11f3fb17a6cca2e9017ec2d6e85c4193e6
raythurman2386 <raymondthurman5@gmail.com>
2019-08-28 19:23:55 -0400

    tested tailwind and set up hooks for data

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Tailwind Styles
+import './output.css';
 
 function App() {
+  // Data for the axios call
+  const [data, setData] = useState();
+
+  // useEffect to grab the data
+  useEffect(() => {
+    axios
+      .get()
+      .then()
+      .catch();
+  });
+
   return (
-    <div className="App">
+    <div className='bg-blue-700'>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
