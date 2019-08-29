576006ecc0d228a54fb53f86c95a976fbb33f932
raythurman2386 <raymondthurman5@gmail.com>
2019-08-28 22:35:07 -0400

    title fixed

--- src/components/Maincontent/Title/Title.js
+++ src/components/Maincontent/Title/Title.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const Title = () => {
-  <div className='flex justify-center h-screen'>
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun 🚀!
-    </p>
-  </div>;
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
 };
 
 export default Title;
