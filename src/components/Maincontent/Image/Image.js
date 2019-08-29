7469871a44d057e251c77115018a9d7185cf883f
raythurman2386 <raymondthurman5@gmail.com>
2019-08-29 19:14:37 -0400

    adding styles

--- src/components/Maincontent/Image/Image.js
+++ src/components/Maincontent/Image/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Image = ({ image }) => {
   return (
-    <div className='my-6'>
+    <div className='flex justify-center my-6'>
       <img src={image} alt='NASA Images' />
     </div>
   );
--- src/components/Maincontent/Video/Video.js
+++ src/components/Maincontent/Video/Video.js
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player';
 
 const Video = ({ video }) => {
   return (
-    <div>
+    <div className='my-6'>
       <ReactPlayer url={video} playing controls width='80%' />
     </div>
   );
