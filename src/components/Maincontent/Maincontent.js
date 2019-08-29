17727ee85c981b8c0b3c4eff51981f6ebe27ffb4
raythurman2386 <raymondthurman5@gmail.com>
2019-08-29 11:31:58 -0400

    props passed to maincontent

--- src/components/Maincontent/Maincontent.js
+++ src/components/Maincontent/Maincontent.js
@@ -6,22 +6,22 @@ import Image from './Image/Image';
 import Video from './Video/Video';
 import Description from './Description/Description';
 
-const Maincontent = () => {
+const Maincontent = ({ title, date, url, mediaType, description }) => {
   // try to implement loader here
-  // if (!data)
-  //   return (
-  //     <div>
-  //       <p>Loading . . .</p>
-  //     </div>
-  //   );
+  if (!data)
+    return (
+      <div>
+        <p>Loading . . .</p>
+      </div>
+    );
 
   return (
     <div className='flex flex-col align-center h-screen'>
-      <Title />
+      <Title title={title} />
       <div className='flex justify-around'>
-        <Image />
-        <Video />
-        <Description />
+        <Image image={url} />
+        <Video video={url} />
+        <Description description={description} />
       </div>
     </div>
   );
