617afffb9ec02cf37951f1e75d1caab51fb4380f
raythurman2386 <raymondthurman5@gmail.com>
2019-08-29 19:46:32 -0400

    video styles fixed

--- src/components/Maincontent/Video/Video.js
+++ src/components/Maincontent/Video/Video.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 const Video = ({ video }) => {
   return (
     <div className='flex justify-center my-6'>
-      <ReactPlayer url={video} playing controls width='80%' />
+      <ReactPlayer url={video} playing controls />
     </div>
   );
 };
