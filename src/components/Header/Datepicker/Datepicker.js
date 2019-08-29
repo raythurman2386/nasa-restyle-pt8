b883429eb156807f6ad9770756ec9ab53f0c4b81
raythurman2386 <raymondthurman5@gmail.com>
2019-08-29 19:45:34 -0400

    styled the input for the datepicker

--- src/components/Header/Datepicker/Datepicker.js
+++ src/components/Header/Datepicker/Datepicker.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-
-// Styles for datepicker
-import 'react-datepicker/dist/react-datepicker.css';
 
 const Datepicker = ({ date, setDate }) => {
   return (
     <form className='flex my-6'>
       <input
+        className='rounded-lg p-1'
         placeholder={date}
         onChange={event => setDate(event.target.value)}
         type={'date'}
